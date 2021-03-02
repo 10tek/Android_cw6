231f9df4f016e7b34428922c45f4c3e1e8f2cb6b
10tekinvo@gmail.com <10tekinvo@gmail.com>
2021-03-02 20:05:55 +0600

    <feat> ($AboutActivity): add btn variable and initializer functions add text_view to xml

--- app/src/main/java/kz/step/cw6/AboutActivity.kt
+++ app/src/main/java/kz/step/cw6/AboutActivity.kt
@@ -1,11 +1,29 @@
 package kz.step.cw6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AboutActivity : AppCompatActivity() {
+    var aboutText: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        initializeViews()
+        initializeListeners()
+    }
+
+
+    private fun initializeViews() {
+        aboutText = findViewById(R.id.text_view_about_activity_about)
+    }
+
+    private fun initializeListeners() {
+        aboutText?.setOnClickListener {
+
+        }
+
     }
 }
