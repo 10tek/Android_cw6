fb914c26a228fc963b18b233f7d7d7040ca309de
10tekinvo@gmail.com <10tekinvo@gmail.com>
2021-03-02 19:58:30 +0600

    <feat> ($MainActivity): add btn variable and initialize functions add a transition to the second window

--- app/src/main/java/kz/step/cw6/MainActivity.kt
+++ app/src/main/java/kz/step/cw6/MainActivity.kt
@@ -1,11 +1,30 @@
 package kz.step.cw6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+    var aboutButton: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeViews()
+        initializeLiseners()
+    }
+
+    private fun initializeViews() {
+        aboutButton = findViewById(R.id.button_main_activity_about)
+    }
+
+    private fun initializeLiseners() {
+        aboutButton?.setOnClickListener {
+            val intent = Intent(this@MainActivity, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
